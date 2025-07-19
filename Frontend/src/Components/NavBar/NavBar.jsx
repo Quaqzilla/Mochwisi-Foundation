1653d75c9cefc013cc8be36ee5dc60407bff090c
Quaqzilla <bbrigett207@gmail.com>
2025-07-19 15:09:54 +0200

    Navigation updates

--- Frontend/src/Components/NavBar/NavBar.jsx
+++ Frontend/src/Components/NavBar/NavBar.jsx
@@ -5,22 +5,48 @@ import logo from './../../assets/logo.png';
 
 const NavBar = () => {
 
+    const [isActive, setIsActive] = useState(false);
+
+    const toggleMenu = () => {
+        setIsActive(prev => !prev);
+    };
+
     return(
         <div className="main-nav">
 
-            <img src={logo} alt="business-logo" />
+                <img src={logo} alt="business-logo" />
 
-            <div className="nav-links">
-                <ul>
-                    <li>ABOUT US</li>
-                    <li>WHAT WE DO</li>
-                    <li>ADDITIONAL</li>
-                </ul>
-            </div>
+                <div className="nav-links">
+                    <ul>
+                        <li>ABOUT US</li>
+                        <li>WHAT WE DO</li>
+                        <li>ADDITIONAL</li>
+                    </ul>
+                </div>
 
-            <button className="contact">
-                Contact US
-            </button>
+                <button className="contact">
+                    Contact US
+                </button>
+            
+                <div className="small-screens">
+
+                    <div class={`off-screen-menu ${isActive ? "active" : ""}`}>
+                        <ul>
+                            
+                        </ul>
+                    </div>
+
+                    <nav>
+                        <div class={`ham-menu ${isActive ? "active" : ""}`} onClick={toggleMenu}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    </nav>
+
+                </div>
+            
+            
 
         </div>
     )
